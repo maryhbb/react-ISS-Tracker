5b168fa95f17832698dc0d36118518d85429e233
Mary <mary_hbb@yahoo.com>
2024-02-04 19:31:39 +0100

    Add config

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import Controls from "./components/Controls";
+import Map from "./components/Map";
+import "./styles.css";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export default function App() {
+  const [coords, setCoords] = useState({
+    longitude: 0,
+    latitude: 0,
+  });
+
+  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      if (response.ok) {
+        const data = await response.json();
+        setCoords({ longitude: data.longitude, latitude: data.latitude });
+      } else {
+        console.error("Failed to fetch ISS coordinates");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    const timer = setInterval(getISSCoords, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <main>
+      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Controls
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+        onRefresh={getISSCoords}
+      />
+    </main>
+  );
+}
